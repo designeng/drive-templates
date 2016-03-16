39aee62a12195520ef0b173185cd035d95ccdede
designeng <denissavenok@gmail.com>
2016-03-16 13:22:58 +0300

    removed build

--- build/delimiter.js
+++
@@ -1,3 +0,0 @@
-{"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<i class=\"i\"></i>";
-},"useData":true}
--- build/logo.js
+++
@@ -1,3 +0,0 @@
-{"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<a id=\"logo\" href=\"/\">ДРАЙВ</a>";
-},"useData":true}
