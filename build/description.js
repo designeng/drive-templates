8837835e17497c0673dc334b3da5ad1441e94c8e
designeng <denissavenok@gmail.com>
2016-03-16 13:33:17 +0300

    removed build: test

--- build/description.js
+++
@@ -1,7 +0,0 @@
-{"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    var helper;
-
-  return "<meta name=\"description\" content=\""
-    + this.escapeExpression(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper)))
-    + "\"/>";
-},"useData":true}
