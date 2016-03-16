a72daa1962c03d4f6dd952b602c8046c5cb8ef35
designeng <denissavenok@gmail.com>
2016-03-16 12:57:50 +0300

    path fixed

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,7 @@ var gulp = require('gulp');
 var handlebars = require('gulp-handlebars');
  
 gulp.task('build', function() {
-    console.log("building...");
     gulp.src('blocks/**/*.hbs')
         .pipe(handlebars())
-        .pipe(gulp.dest('build/js/'));
+        .pipe(gulp.dest('build/'));
 });
