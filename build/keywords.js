3a6434d87dfb5df04ba8b005a8a44e9d051bb187
designeng <denissavenok@gmail.com>
2016-03-16 13:32:35 +0300

    build

---
+++ build/keywords.js
@@ -0,0 +1,7 @@
+{"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var helper;
+
+  return "<meta name=\"keywords\" content=\""
+    + this.escapeExpression(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper)))
+    + "\"/>";
+},"useData":true}
---
+++ build/mobileMenuTrigger.js
@@ -0,0 +1,3 @@
+{"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<input type=\"checkbox\" id=\"nav-mobile-trigger\" class=\"nav-mobile-trigger\">\n<label for=\"nav-mobile-trigger\" class=\"nav-mobile-trigger-label\"></label>";
+},"useData":true}
