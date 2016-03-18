93b2b7db64b24f6651d1507efbadebe606888753
designeng <denissavenok@gmail.com>
2016-03-18 15:13:02 +0300

    med -> medium; {{{ capture }}}

--- build/itemLarge.js
+++ build/itemLarge.js
@@ -1,5 +1,5 @@
 module.exports = require('handlebars').template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+    var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
   return "<a href=\""
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
@@ -8,6 +8,6 @@ module.exports = require('handlebars').template({"compiler":[7,">= 4.0.0"],"main
     + "\">\n    <div class=\"image-wrapper image-wrapper_big\">\n        <img class=\"top-news-item__image\" src=\""
     + alias4(((helper = (helper = helpers.imageUrl || (depth0 != null ? depth0.imageUrl : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"imageUrl","hash":{},"data":data}) : helper)))
     + "\" width=\"460\" height=\"260\" alt=\"\">\n    </div>\n    <i class=\"i vgr\"></i>\n    <strong class=\"top-news-item__caption\">\n        "
-    + alias4(((helper = (helper = helpers.caption || (depth0 != null ? depth0.caption : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"caption","hash":{},"data":data}) : helper)))
+    + ((stack1 = ((helper = (helper = helpers.caption || (depth0 != null ? depth0.caption : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"caption","hash":{},"data":data}) : helper))) != null ? stack1 : "")
     + "\n    </strong>\n    <ins></ins>\n</a>";
 },"useData":true})
--- build/itemMedium.js
+++ build/itemMedium.js
@@ -1,5 +1,5 @@
 module.exports = require('handlebars').template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+    var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
   return "<a class=\"news-item ncard "
     + alias4(((helper = (helper = helpers.shadowClass || (depth0 != null ? depth0.shadowClass : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"shadowClass","hash":{},"data":data}) : helper)))
@@ -8,6 +8,6 @@ module.exports = require('handlebars').template({"compiler":[7,">= 4.0.0"],"main
     + "\">\n    <div class=\"news-item-link\">\n        <img class=\"news-item-link__image\" src=\""
     + alias4(((helper = (helper = helpers.imageUrl || (depth0 != null ? depth0.imageUrl : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"imageUrl","hash":{},"data":data}) : helper)))
     + "\" width=\"230\" height=\"130\">\n    </div>\n    <i class=\"i\"></i>\n    <div class=\"news-item-caption\">\n        <strong class=\"news-item-caption__text\">"
-    + alias4(((helper = (helper = helpers.caption || (depth0 != null ? depth0.caption : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"caption","hash":{},"data":data}) : helper)))
+    + ((stack1 = ((helper = (helper = helpers.caption || (depth0 != null ? depth0.caption : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"caption","hash":{},"data":data}) : helper))) != null ? stack1 : "")
     + "</strong>\n        <ins></ins>\n    </div>\n</a>";
 },"useData":true})
